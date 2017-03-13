4c2391f51fa7dc85b059a00a17dc47347fb6e480
Łukasz <lukaslizon@gmail.com>
2017-03-14 00:45:51 +0100

    Login page with login logic

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -3,23 +3,51 @@ import React, { PureComponent, PropTypes } from 'react'
 import bemClassname from 'bem-classname'
 // STYLES
 import './LoginPage.scss'
-// CONSTS
-import { user } from '../../../private/config'
+// COMPONENTS
+import LoginInput from './LoginInput/LoginInput'
+// RECOURCES
+import background from '../../assets/images/background2.png'
 
 class LoginPage extends PureComponent {
   constructor () {
     super()
     this.className = bemClassname.bind(null, 'LoginPage')
+    this.state = {
+      email: '',
+      password: ''
+    }
   }
 
-  loginUser () {
-    this.props.loginUser(user)
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.loginUser(this.state)
+  }
+
+  handleChange (key, value) {
+    this.setState({
+      [key]: value
+    })
   }
 
   render () {
+    const bgStyle = {backgroundImage: `url(${background})`}
     return (
-      <div className={this.className()}>
-        <div onClick={this.loginUser.bind(this)}>LOGIN</div>
+      <div className={this.className()} style={bgStyle}>
+        <form className={this.className('loginForm')} onSubmit={this.handleSubmit.bind(this)}>
+          <LoginInput
+            type='email'
+            handleChange={this.handleChange.bind(this)}
+            name='email'
+            value={this.state.email}
+            label='email' />
+          <LoginInput
+            type='password'
+            handleChange={this.handleChange.bind(this)}
+            name='password'
+            value={this.state.password}
+            label='password' />
+          <button className={this.className('loginBtn')}>LOGIN</button>
+        </form>
       </div>
     )
   }
